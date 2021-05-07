c92e8f4744c83e3419e37220b54b457f76a3d3df
devrshidmodi <76793692+devrshidmodi@users.noreply.github.com>
2021-05-06 23:36:57 -0400

    Add files via upload

---
+++ BabylonianForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace BabylonianMethod
+{
+    partial class BabylonianMethodForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.BabylonianDataGridView = new System.Windows.Forms.DataGridView();
+            this.nColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qDividedByQnColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.differenceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.qTextBox = new System.Windows.Forms.TextBox();
+            this.SquareRootButton = new System.Windows.Forms.Button();
+            this.SquareRootLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BabylonianDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BabylonianDataGridView
+            // 
+            this.BabylonianDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightCyan;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.BabylonianDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.BabylonianDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.BabylonianDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nColumn,
+            this.qn,
+            this.qDividedByQnColumn,
+            this.differenceColumn});
+            this.BabylonianDataGridView.EnableHeadersVisualStyles = false;
+            this.BabylonianDataGridView.Location = new System.Drawing.Point(12, 104);
+            this.BabylonianDataGridView.Name = "BabylonianDataGridView";
+            this.BabylonianDataGridView.ReadOnly = true;
+            this.BabylonianDataGridView.RowHeadersVisible = false;
+            this.BabylonianDataGridView.Size = new System.Drawing.Size(549, 283);
+            this.BabylonianDataGridView.TabIndex = 0;
+            // 
+            // nColumn
+            // 
+            this.nColumn.HeaderText = "n";
+            this.nColumn.Name = "nColumn";
+            this.nColumn.ReadOnly = true;
+            // 
+            // qn
+            // 
+            this.qn.HeaderText = "qn";
+            this.qn.Name = "qn";
+            this.qn.ReadOnly = true;
+            // 
+            // qDividedByQnColumn
+            // 
+            this.qDividedByQnColumn.HeaderText = "q Divided by qn";
+            this.qDividedByQnColumn.Name = "qDividedByQnColumn";
+            this.qDividedByQnColumn.ReadOnly = true;
+            // 
+            // differenceColumn
+            // 
+            this.differenceColumn.HeaderText = "Difference (qn - q/qn)";
+            this.differenceColumn.Name = "differenceColumn";
+            this.differenceColumn.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter any nonnegative value:";
+            // 
+            // qTextBox
+            // 
+            this.qTextBox.Location = new System.Drawing.Point(164, 25);
+            this.qTextBox.Name = "qTextBox";
+            this.qTextBox.Size = new System.Drawing.Size(220, 20);
+            this.qTextBox.TabIndex = 2;
+            // 
+            // SquareRootButton
+            // 
+            this.SquareRootButton.Location = new System.Drawing.Point(390, 23);
+            this.SquareRootButton.Name = "SquareRootButton";
+            this.SquareRootButton.Size = new System.Drawing.Size(171, 23);
+            this.SquareRootButton.TabIndex = 3;
+            this.SquareRootButton.Text = "Calculate Square Root";
+            this.SquareRootButton.UseVisualStyleBackColor = true;
+            this.SquareRootButton.Click += new System.EventHandler(this.SquareRootButton_Click);
+            // 
+            // SquareRootLabel
+            // 
+            this.SquareRootLabel.AutoSize = true;
+            this.SquareRootLabel.Location = new System.Drawing.Point(12, 67);
+            this.SquareRootLabel.Name = "SquareRootLabel";
+            this.SquareRootLabel.Size = new System.Drawing.Size(0, 13);
+            this.SquareRootLabel.TabIndex = 4;
+            // 
+            // BabylonianMethodForm
+            // 
+            this.AcceptButton = this.SquareRootButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 394);
+            this.Controls.Add(this.SquareRootLabel);
+            this.Controls.Add(this.SquareRootButton);
+            this.Controls.Add(this.qTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BabylonianDataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "BabylonianMethodForm";
+            this.Text = "Babylonian Method for Approximating Square Roots";
+            ((System.ComponentModel.ISupportInitialize)(this.BabylonianDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView BabylonianDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qDividedByQnColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn differenceColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox qTextBox;
+        private System.Windows.Forms.Button SquareRootButton;
+        private System.Windows.Forms.Label SquareRootLabel;
+    }
+}
+
---
+++ BabylonianForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace BabylonianMethod
+{
+    public partial class BabylonianMethodForm : Form
+    {
+        /*
+         * The following method is a "constructor method" for the class 'BabylonianMethodForm'.
+         * This method is called whenever a new instance of thie class is created (i.e. a new form).
+         * See the file 'Program.cs' for the statement that creates a new form object using the class
+         * 'BabylonianMethodForm' and then runs the application. 
+         * ( Statement is 'Application.Run(new BabylonianMethodForm());' )
+         */
+
+        public BabylonianMethodForm()
+        {
+            // Call object initialization method in 'BabylonianForm.Designer.cs'
+            InitializeComponent();
+
+            // The keyword 'this' is a placeholder for the object name. See https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/this
+            // Pressing the 'ENTER' key is equivalent to clicking 'SquareRootButton'
+            this.AcceptButton = SquareRootButton;
+
+            // The control 'BabylonianDataGridView' has been included mostly for debugging purposes. 
+            // You are NOT required to master how to use a DataGridView control. For those of you who are
+            // interested, more info can be found on the Web pages listed below:
+            // https://stackoverflow.com/questions/32576160/can-i-use-datagridview-without-using-database-data
+            // https://stackoverflow.com/questions/2154154/datagridview-how-to-set-column-width
+            // https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datagridviewcolumn.fillweight?redirectedfrom=MSDN&view=netframework-4.8#System_Windows_Forms_DataGridViewColumn_FillWeight
+            // http://csharp.net-informations.com/datagridview/csharp-datagridview-tutorial.htm
+
+            /* Set important properties of 'BabylonianDataGridView'.
+             * Many (if not all) of these properties can be set through the form designer.
+             * However, doing so would lack transparency. That is, someone unfamiliar with the app
+             * would have a difficult time understanding how various things were accomplished.
+             */
+            
+            // This property must be set to 'false' to allow for various customizations of 'BabylonianDataGridView'
+            BabylonianDataGridView.EnableHeadersVisualStyles = false;
+
+            /* 
+             * Set properties relating to the appearance of 'BabylonianDataGridView'.
+             */ 
+            BabylonianDataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.LightCyan;
+            BabylonianDataGridView.ColumnHeadersDefaultCellStyle.ForeColor = Color.Blue;
+            BabylonianDataGridView.ColumnHeadersDefaultCellStyle.Font
+                                                    = new Font("Arial", 12, FontStyle.Bold, GraphicsUnit.Pixel);
+            BabylonianDataGridView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            BabylonianDataGridView.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // Columns fill entire width of 'BabylonianDataGridView'
+            BabylonianDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Set relative column widths e.g. column 0 width = 1/(1+5+5+5) = 1/16th of total width
+            BabylonianDataGridView.Columns[0].FillWeight = 1;
+            BabylonianDataGridView.Columns[1].FillWeight = 5;
+            BabylonianDataGridView.Columns[2].FillWeight = 5;
+            BabylonianDataGridView.Columns[3].FillWeight = 5;
+
+            // Row headers not needed
+            BabylonianDataGridView.RowHeadersVisible = false;
+
+            // Disallow editing since the DataGridView control is being used purely for output purposes.
+            BabylonianDataGridView.ReadOnly = true;
+        }
+
+        /*
+         * This procedure has certain nasty bugs that were included intentionally.
+         * Your job is to identify, explain, and correct the bugs.
+         */
+        private void SquareRootButton_Click(object sender, EventArgs e)
+        {
+            // Clear any existing rows from 'BabylonianDataGridView'.
+            BabylonianDataGridView.Rows.Clear();
+            BabylonianDataGridView.Refresh();
+
+            int n = 1;
+
+            double q = Convert.ToDouble(qTextBox.Text);
+            double qN = q / 2; // Simple way to calculate q1 (first term in sequence)
+            double qDividedByQn = q / qN;
+            double b = 0;
+            double difference = qN - qDividedByQn;
+            
+            // Add a row to 'BabylonianDataGridView' of initial values.
+            BabylonianDataGridView.Rows.Add(new string[] {n.ToString(), qN.ToString(),
+                                                          qDividedByQn.ToString(), difference.ToString() });
+
+            while (b - difference != 0)
+            {
+                qN = (qN + qDividedByQn) / 2;
+                qDividedByQn = q / qN;
+                n++;
+                b = difference;
+                difference = qN - qDividedByQn;
+
+                // Add a row to 'BabylonianDataGridView' for iteration 'n'.
+                BabylonianDataGridView.Rows.Add(new string[] {n.ToString(), qN.ToString(),
+                                                          qDividedByQn.ToString(), difference.ToString() });
+
+                Application.DoEvents(); // Process the event queue to prevent application hang.
+            }
+
+            SquareRootLabel.Text = "The square root of " + q.ToString() + " is approximately " + qN.ToString() + ".";
+        }
+    }
+
+
+}
